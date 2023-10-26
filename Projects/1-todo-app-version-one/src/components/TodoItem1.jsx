1b1a71d9679f3f16c8d86be545257a47f9441a5b
Prashant Jain <prashant@knowledgegate.in>
2023-10-26 16:44:02 +0530

    update

--- Projects/1-todo-app-version-one/src/components/TodoItem1.jsx
+++ Projects/1-todo-app-version-one/src/components/TodoItem1.jsx
@@ -1,19 +1,24 @@
+import React from "react";
+
 function TodoItem1() {
   let todoName = "Buy Milk";
   let todoDate = "4/10/2023";
 
   return (
-    <div class="container">
-      <div class="row kg-row">
-        <div class="col-6">{todoName}</div>
-        <div class="col-4">{todoDate}</div>
-        <div class="col-2">
-          <button type="button" class="btn btn-danger kg-button">
-            Delete
-          </button>
+    <React.Fragment>
+      <div class="container">
+        <div class="row kg-row">
+          <div class="col-6">{todoName}</div>
+          <div class="col-4">{todoDate}</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger kg-button">
+              Delete
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <p>ddd</p>
+    </React.Fragment>
   );
 }
 
