bf0f11d3a8ca001647d972dfd6ba6266580d9b24
Prashant Jain <prashant@knowledgegate.in>
2023-11-03 09:24:00 +0530

    Final Project Pre-reqs

---
+++ Projects/9-Myntra Clone/old-clone/scripts/bag.js
@@ -0,0 +1,106 @@
+const CONVENIENCE_FEES = 99;
+let bagItemObjects;
+onLoad();
+
+function onLoad() {
+  loadBagItemObjects();
+  displayBagItems();
+  displayBagSummary();
+}
+
+function displayBagSummary() {
+  let bagSummaryElement = document.querySelector('.bag-summary');
+  let totalItem = bagItemObjects.length;
+  let totalMRP = 0;
+  let totalDiscount = 0;
+
+  bagItemObjects.forEach(bagItem => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price;
+  });
+
+  let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
+  
+
+  bagSummaryElement.innerHTML = `
+    <div class="bag-details-container">
+    <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
+    <div class="price-item">
+      <span class="price-item-tag">Total MRP</span>
+      <span class="price-item-value">₹${totalMRP}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Discount on MRP</span>
+      <span class="price-item-value priceDetail-base-discount">-₹${totalDiscount}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Convenience Fee</span>
+      <span class="price-item-value">₹99</span>
+    </div>
+    <hr>
+    <div class="price-footer">
+      <span class="price-item-tag">Total Amount</span>
+      <span class="price-item-value">₹${finalPayment}</span>
+    </div>
+  </div>
+  <button class="btn-place-order">
+    <div class="css-xjhrni">PLACE ORDER</div>
+  </button>
+  `;
+}
+
+function loadBagItemObjects() {
+  console.log(bagItems);
+  bagItemObjects = bagItems.map(itemId => {
+    for (let i = 0; i < items.length; i++) {
+      if (itemId == items[i].id) {
+        return items[i];
+      }
+    }
+  });
+  console.log(bagItemObjects);
+}
+
+function displayBagItems() {
+  let containerElement = document.querySelector('.bag-items-container');
+  let innerHTML = '';
+  bagItemObjects.forEach(bagItem => {
+    innerHTML += generateItemHTML(bagItem);
+  });
+  containerElement.innerHTML = innerHTML;
+}
+
+function removeFromBag(itemId) {
+  bagItems = bagItems.filter(bagItemId => bagItemId != itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  loadBagItemObjects();
+  displayBagIcon();
+  displayBagItems();
+  displayBagSummary();
+}
+
+function generateItemHTML(item) {
+  return `<div class="bag-item-container">
+    <div class="item-left-part">
+      <img class="bag-item-img" src="../${item.image}">
+    </div>
+    <div class="item-right-part">
+      <div class="company">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price-container">
+        <span class="current-price">Rs ${item.current_price}</span>
+        <span class="original-price">Rs ${item.original_price}</span>
+        <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <div class="return-period">
+        <span class="return-period-days">${item.return_period} days</span> return available
+      </div>
+      <div class="delivery-details">
+        Delivery by
+        <span class="delivery-details-days">${item.delivery_date}</span>
+      </div>
+    </div>
+
+    <div class="remove-from-cart" onclick="removeFromBag(${item.id})">X</div>
+  </div>`;
+}
---
+++ Projects/9-Myntra Clone/old-clone/scripts/index.js
@@ -0,0 +1,52 @@
+let bagItems;
+onLoad();
+
+function onLoad() {
+  let bagItemsStr = localStorage.getItem('bagItems');
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
+}
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if (bagItems.length > 0) {
+    console.log('I am here');
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = bagItems.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  }
+}
+
+function displayItemsOnHomePage() {
+  let itemsContainerElement = document.querySelector('.items-container');
+  if (!itemsContainerElement) {
+    return;
+  }
+  let innerHtml = '';
+  items.forEach(item => {
+    innerHtml += `
+    <div class="item-container">
+      <img class="item-image" src="${item.image}" alt="item image">
+      <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+    </div>`
+  });
+  itemsContainerElement.innerHTML = innerHtml;
+}
