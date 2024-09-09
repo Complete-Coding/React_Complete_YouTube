c8f2c9a6505e301393af22c2a348b0d8acaf5c0d
Harshendra M <151964309+harshendram@users.noreply.github.com>
2024-09-10 03:24:58 +0530

    Update App.jsx

--- Projects/8-bharat-clock-version-two/src/App.jsx
+++ Projects/8-bharat-clock-version-two/src/App.jsx
@@ -2,7 +2,7 @@ import ClockHeading from "./components/ClockHeading";
 import ClockSlogan from "./components/ClockSlogan";
 import CurrentTime from "./components/CurrentTime";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+
 
 function App() {
   return (
